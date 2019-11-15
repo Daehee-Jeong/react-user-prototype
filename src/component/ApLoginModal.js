b30beb276ce58ffbc793c2e2c8ca8d8b941019c9
Daehee-Jeong <dev.james.jeong@gmail.com>
2019-11-15 16:41:36 +0900

    ApLoginModal validation 로직 추가

--- src/component/ApLoginModal.js
+++ src/component/ApLoginModal.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
-
 import {
-    Button, Modal, ModalHeader, ModalBody, ModalFooter
+    Button, 
+    Modal, 
+    ModalHeader, 
+    ModalBody, 
+    ModalFooter, 
+    Form, 
+    FormGroup, 
+    Col, 
+    Label, 
+    Input
 } from 'reactstrap';
+import * as service from '../services/user.js';
 
 class ApLoginModal extends Component {
 
@@ -10,19 +19,108 @@ class ApLoginModal extends Component {
         super(props);
         this.state = { modal: this.props.modal }    
         this.toggle = this.props.toggle.bind(this);
+
+        this.state = {
+            idValid: false,
+            idEntered: '',
+            pwValid: false,
+            pwEntered: ''
+        }
+    }
+
+    fetchUserId = async (userId) => {
+        const _userId = await service.getUserId(userId);
+        console.log(_userId);
+    }
+
+    validateId = (idEntered) => {
+        const emailRegExp = /^[\w-]+(\.[\w-]+)*@([a-z0-9-]+(\.[a-z0-9-]+)*?\.[a-z]{2,6}|(\d{1,3}\.){3}\d{1,3})(:\d{4})?$/;
+
+        if (idEntered.match(emailRegExp)) {
+          this.setState({
+            idValid: true,
+            idEntered
+          });
+        } else {
+          this.setState({
+            idValid: false,
+            idEntered
+          });
+        }
+    }
+
+    isEnteredIdValid = () => {
+        const { idEntered, idValid } = this.state;
+        if (idEntered) return idValid;
+    }
+
+    validatePassword = (pwEntered) => {
+        if (pwEntered.length > 7) {
+            this.setState({
+                pwValid: true,
+                pwEntered
+            });
+        } else {
+            this.setState({
+                pwValid: false,
+                pwEntered
+            })
+        }
+    }
+
+    isEnteredPwValid = () => {
+        const { pwEntered, pwValid } = this.state;
+        if (pwEntered) return pwValid;
+    }
+
+    inputClassNameHelper = (booleanValue) => {
+        switch (booleanValue) {
+            case true:
+                return 'is-valid';
+            case false:
+                return 'is-invalid';
+            default:
+                return '';
+        }
     }
 
     render() {
         return (
             <div>
                 <Modal isOpen={this.props.modal} toggle={this.toggle} className={this.state.className}>
-                <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+                <ModalHeader toggle={this.toggle}>로그인</ModalHeader>
                 <ModalBody>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <Form>
+                    <FormGroup row>
+                        <Label for="exampleEmail" sm={2}>Email</Label>
+                        <Col sm={10}>
+                        <Input 
+                            type="email"
+                            name="email"
+                            id="exampleEmail"
+                            placeholder="이메일 주소를 입력하세요"
+                            valid={this.state.idValid}
+                            className={`form-control ${this.inputClassNameHelper(this.isEnteredIdValid())}`}
+                            onChange={e => this.validateId(e.target.value)} />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label for="examplePassword" sm={2}>Password</Label>
+                        <Col sm={10}>
+                        <Input
+                            type="password"
+                            name="password"
+                            id="examplePassword"
+                            placeholder="비밀번호를 입력하세요"
+                            className={`form-control ${this.inputClassNameHelper(this.isEnteredPwValid())}`}
+                            onChange={e => this.validatePassword(e.target.value)} />
+                        </Col>
+                    </FormGroup>
+                </Form>
                 </ModalBody>
                 <ModalFooter>
-                    <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-                    <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                    <Button color="primary" onClick={ () => {this.fetchUserId('daehee');} }>로그인</Button>{' '}
+                    <Button color="secondary" onClick={this.toggle}>회원가입</Button>
                 </ModalFooter>
                 </Modal>
             </div>
---
+++ src/services/user.js
@@ -0,0 +1,6 @@
+// import axios from 'axios';
+
+export function getUserId(userId) {
+    // return axios.get('https://someurl' + userId);
+    return userId;
+}
