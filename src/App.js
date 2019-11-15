1b6e08a0eeb6b26d79590c9930aa7f897ae1c523
Daehee-Jeong <dev.james.jeong@gmail.com>
2019-11-15 12:19:40 +0900

    Navbar -> ApNavbar 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,73 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import './component/ApNavbar.js';
 
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
-} from 'reactstrap';
+import ApNavbar from './component/ApNavbar';
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isOpen: false
-    }
-
-    this.toggle = this.toggle.bind(this);
-  }
-
-  toggle = () => {
-    this.setState({
-      isOpen: !this.state.isOpen
-    })
-  }
 
   render = () => {
     return (
       <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <NavbarToggler onClick={this.state.toggle} />
-        <Collapse isOpen={this.state.isOpen} navbar>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    </div>
+        <ApNavbar></ApNavbar>
+      </div>
     );
   }
 }
---
+++ src/component/ApNavbar.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+
+class ApNavbar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: false
+        }
+
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
+    render() {
+        return (
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">reactstrap</NavbarBrand>
+                <NavbarToggler onClick={this.state.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                    <NavLink href="/components/">Components</NavLink>
+                    </NavItem>
+                    <NavItem>
+                    <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                    </NavItem>
+                    <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                        Options
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                        <DropdownItem>
+                        Option 1
+                        </DropdownItem>
+                        <DropdownItem>
+                        Option 2
+                        </DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem>
+                        Reset
+                        </DropdownItem>
+                    </DropdownMenu>
+                    </UncontrolledDropdown>
+                </Nav>
+                </Collapse>
+            </Navbar>
+        )
+    }
+}
+
+export default ApNavbar;
