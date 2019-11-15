6f9ff4a66efb28c67926ddf0ea9c91ccf1d463ce
Daehee-Jeong <dev.james.jeong@gmail.com>
2019-11-15 14:40:47 +0900

    ApLoginModal 호출 위치를 App으로 이동

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+
+import {
+  Button
+} from 'reactstrap';
+
 import './App.css';
 import './component/ApNavbar.js';
 
@@ -6,12 +11,28 @@ import ApNavbar from './component/ApNavbar';
 import ApLoginModal from './component/ApLoginModal';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+      
+    this.state = {
+      loginModal: false
+    }
+
+    this.toggleLoginModal = this.toggleLoginModal.bind(this);
+  }
+
+  toggleLoginModal = () => {
+    this.setState({
+      loginModal: !this.state.loginModal
+    })
+  }
 
   render = () => {
     return (
       <div>
         <ApNavbar></ApNavbar>
-        <ApLoginModal></ApLoginModal>
+        <Button color="danger" onClick={this.toggleLoginModal}>로그인</Button>
+        <ApLoginModal modal={this.state.loginModal} toggle={this.toggleLoginModal}></ApLoginModal>
       </div>
     );
   }
